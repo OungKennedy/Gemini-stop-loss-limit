1e0c1b66e2bd43edb9201d734792577055b6db15
John Pope <jp@bellgeorge.com>
2018-08-29 01:06:38 -0400

    make loop hit api to check latest price (#2)

--- Gemini shortstop.py
+++ Gemini shortstop.py
@@ -8,6 +8,7 @@ api_key = ''
 api_secret = ''
 #for real trading account, change sandbox = True to sandbox = False
 gemi = Client(api_key, api_secret, sandbox=True)
+all_is_well = True
 
 def check_price(currency , shortstop_price , sell_price , sell_amount):
     '''
@@ -28,23 +29,23 @@ def check_price(currency , shortstop_price , sell_price , sell_amount):
     time_now = str(datetime.now())
     #client order id: set to be date and time when order is placed
     order_id = strftime('%m-%d_%H:%M',gmtime())
-    all_is_well = True
-    while all_is_well:
-        if shortstop_price:
-            if trade_price <= shortstop_price:
-                gemi.new_order(symbol=currency, amount=sell_amount, price=sell_price,
-                               side='sell', type = 'exchange limit', client_order_id = order_id)
-                print('stop limit for '+currency+' triggered at '+time_now+'.'+ str(sell_amount)+currency+' sold.')
-                all_is_well = False
-            else:
-                print(currency+':'+trade_price+' at '+ time_now)
+    if shortstop_price:
+        if trade_price <= shortstop_price:
+            gemi.new_order(symbol=currency, amount=sell_amount, price=sell_price,
+                            side='sell', type = 'exchange limit', client_order_id = order_id)
+            print('stop limit for '+currency+' triggered at '+time_now+'.'+ str(sell_amount)+currency+' sold.')
+            all_is_well = False
+        else:
+            print(str(currency)+':'+str(trade_price)+' at '+ time_now)
         #enter the period (in seconds) how often you'd like the bot to check the prices.
         #e.g. sleep(30) would mean checking every 30 secs
-        sleep(300)
+        
 
 #change the numbers in check_price below to set the stop loss you want.
 def trade():
-        check_price('btcusd', shortstop_price=4000, sell_price=4100,sell_amount=0.3)
+    while all_is_well:
+        check_price('ethusd', shortstop_price=4000, sell_price=4100,sell_amount=0.3)
+        sleep(30)
 #press f5 to begin the stop loss program
 trade()
 
